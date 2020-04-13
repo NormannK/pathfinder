029f18ca6cf76bd5286205d45c173bacb03e0a1c
NormannK <github@koldrack.com>
2020-04-13 15:27:23 +0200

    show region under the systems

--- js/app/map/system.js
+++ js/app/map/system.js
@@ -792,6 +792,8 @@ define([
                     '">' + staticData.security + '</span>'
                 );
             }
+        } else {
+            headInfoRight.push(data.region.name + ' ');
         }
 
         if(headInfoLeft.length || headInfoRight.length){
